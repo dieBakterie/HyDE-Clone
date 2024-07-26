a572a4555cd69daa7fffe793c52177bc46320dbe
dieBakterie <diebakterie04@gmail.com>
2024-07-26 11:39:22 +0200

    Aktualisierte monitor und workspace konfiguration

--- Configs/.local/share/bin/wallbashswaync.sh
+++ Configs/.local/share/bin/wallbashswaync.sh
@@ -1,45 +1,43 @@
 #!/usr/bin/env sh
 
 # set variables
-scrDir=$(dirname "$(realpath "$0")")
-source "$scrDir/globalcontrol.sh"
-dstDir="${confDir}/swaync"
-
-# regen conf
-export hypr_border
-envsubst < "${dstDir}/swaync.conf" > "${dstDir}/swaync.con"
-envsubst < "${dstDir}/wallbash.conf" >> "${dstDir}/swaync.con"
-killall swaync.con
-swaync-client -rs
-swaync-client -R
-swaync &
-
-#!/usr/bin/env sh
-
-# set variables
-# scrDir="$(dirname "$(realpath "$0")")"
-# echo "Script directory: $scrDir"
+# scrDir=$(dirname "$(realpath "$0")")
 # source "$scrDir/globalcontrol.sh"
-# echo "Configuration directory: $confDir"
 # dstDir="${confDir}/swaync"
-# echo "Destination directory: $dstDir"
 
 # regen conf
 # export hypr_border
 # envsubst < "${dstDir}/swaync.conf" > "${dstDir}/swaync.con"
 # envsubst < "${dstDir}/wallbash.conf" >> "${dstDir}/swaync.con"
+# killall swaync.con
+# swaync-client -rs
+# swaync-client -R
+# swaync &
+
+# set variables
+scrDir="$(dirname "$(realpath "$0")")"
+echo "Script directory: $scrDir"
+source "$scrDir/globalcontrol.sh"
+echo "Configuration directory: $confDir"
+dstDir="${confDir}/swaync"
+echo "Destination directory: $dstDir"
+
+# regen conf
+export hypr_border
+envsubst < "${dstDir}/swaync.conf" > "${dstDir}/swaync.con"
+envsubst < "${dstDir}/wallbash.conf" >> "${dstDir}/swaync.con"
 
 # Check if swaync process is running
-# if pgrep -x "swaync" > /dev/null
-# then
-#     echo "swaync is running, killing it..."
-#     killall swaync
-# else
-#     echo "swaync is not running"
-# fi
+if pgrep -x "swaync" > /dev/null
+then
+    echo "swaync is running, killing it..."
+    killall swaync
+else
+    echo "swaync is not running"
+fi
 
 # Restart swaync
-# swaync-client -R
-# swaync-client -rs
-# swaync-client -sw
-# swaync &
+swaync-client -R
+swaync-client -rs
+swaync-client -sw
+swaync &
