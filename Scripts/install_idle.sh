0b6e3e87884f7cb9c2370b1c4a1c9e873a96efa7
dieBakterie <diebakterie04@gmail.com>
2024-08-06 20:31:29 +0200

    Update und cleanup

--- Scripts/install_idle.sh
+++ Scripts/install_idle.sh
@@ -56,5 +56,5 @@ else
     }
 
     # Update or add exec-once line in the configuration file
-    update_exec_once "${cloneDir}/Configs/.config/hypr/configs/launchs.conf"
+    update_exec_once "${cloneDir}/Configs/.config/hypr/config/launchs.conf"
 fi
